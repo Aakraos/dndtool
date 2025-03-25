5905954c79b64388f2932d1e8ed6175b4c313055
Aakraos <aakraos@gmail.com>
2025-03-25 02:48:21 +0100

    Version 1.0..4
    Modified vite.config.jsx with the GitHub repository name

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dndtool/',  // Modifica '/dndtool/' con il nome del tuo repository su GitHub
 })
