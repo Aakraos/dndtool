74d545916a203d3e921d5818dcf3ef218a1a6810
Aakraos <aakraos@gmail.com>
2025-03-25 03:43:58 +0100

    1.0.6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dndtool/',  // Modifica '/dndtool/' con il nome del tuo repository su GitHub
-})
+  base: `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`, // Estrae il nome del repo dalla variabile di ambiente
+});
